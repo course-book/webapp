c43917bbfc9470db3010726c19258658efdc5afe
Zixuan Yang <yangz1@rose-hulman.edu>
2018-05-01 17:01:20 -0400

    unstaged change

--- coursebook/src/app/component/create-wish/create-wish.component.ts
+++ coursebook/src/app/component/create-wish/create-wish.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Wish } from '../../_models/wish';
+import { UserService } from '../../_services/user.service';
+import { WishService } from '../../_services/wish.service';
 
 @Component({
   selector: 'app-create-wish',
@@ -6,10 +10,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-wish.component.css']
 })
 export class CreateWishComponent implements OnInit {
+  wish: any={};
+  errmsg: string="";
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private userService: UserService,
+    private wishService: WishService) { }
 
   ngOnInit() {
+  }
+
+
+  updateName(name:string){
+  	this.wish.name = name;
+  	console.log("name: "+this.wish.name);
+  }
+
+  updateDetails(details:string){
+  	this.wish.details = details;
+  	console.log("details: "+ this.wish.details);
+  }
+
+  createWish(){
+    this.wishService.create(this.wish,this.userService.getToken())
+      .subscribe(
+        data => {
+          this.errmsg = data;
+          this.router.navigate(['/home']);
+        },
+        error => {
+          console.log(error.message);
+          this.errmsg = error.error;
+        });
+  }
+
+  onSubmit(){
+  	this.errmsg = "";
+  	if(this.wish.name == ""){
+  		this.errmsg += "Wish name is required. "
+  	}
+  	if(this.wish.details == ""){
+  		this.errmsg += "Wish Details is required. "
+  	}
+  	if(this.errmsg == ""){
+      this.wish.wisher = this.userService.getUser().username;
+      console.log(this.wish.name)
+  	  this.createWish()
+  	}
   }
 
 }
